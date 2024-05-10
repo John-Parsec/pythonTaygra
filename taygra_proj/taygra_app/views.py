7a69040ade81002e5dbbe7b87ff583661303d15c
João Manoel <jmaoliveira.cic@uesc.br>
2024-05-10 11:14:22 -0300

    add views

--- taygra_proj/taygra_app/views.py
+++ taygra_proj/taygra_app/views.py
@@ -8,5 +8,89 @@ from django.apps import apps
 
 # Create your views here.
 
+def context_(request):
+    context = {
+        'user': request.user
+    }
+
+    return context
+
 def home(request):
-    return render(request, 'index.html')
+    context = context_(request)
+
+    return render(request, 'index.html', context=context)
+
+def produtos(request):
+    Produto = apps.get_model('taygra_app', 'Produto')
+    produtos = Produto.objects.all()
+
+    context = context_(request)
+
+    context = {
+        'produtos': produtos
+    }
+
+    return render(request, 'produtos.html', context=context)
+
+def produto(request, produto_id):
+    Produto = apps.get_model('taygra_app', 'Produto')
+    produto = Produto.objects.get(id=produto_id)
+
+    context = context_(request)
+
+    context = {
+        'produto': produto
+    }
+
+    return render(request, 'produto.html', context=context)
+
+def categorias(request):
+    Categoria = apps.get_model('taygra_app', 'Categoria')
+    categorias = Categoria.objects.all()
+
+    context = context_(request)
+
+    context = {
+        'categorias': categorias
+    }
+
+    return render(request, 'categorias.html', context=context)
+
+def categoria(request, categoria_id):
+    Categoria = apps.get_model('taygra_app', 'Categoria')
+    categoria = Categoria.objects.get(id=categoria_id)
+
+    context = context_(request)
+
+    context = {
+        'categoria': categoria
+    }
+
+    return render(request, 'categoria.html', context=context)
+
+def carrinho(request):
+    Pedido = apps.get_model('taygra_app', 'Pedido')
+    pedidos = Pedido.objects.all()
+
+    context = context_(request)
+
+    context = {
+        'pedidos': pedidos
+    }
+
+    return render(request, 'carrinho.html', context=context)
+
+def login(request):
+    if request.method == 'POST':
+        Usuario = apps.get_model('taygra_app', 'Usuario')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        
+    context = {}
+
+    return render(request, 'login.html', context=context)
